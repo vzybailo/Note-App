a7b3bcc0fe6457f26511901df098768879239a69
MAXBIT\v.zybailo <v.zybailo@jetmail.cc>
2023-07-12 17:29:18 +0300

    add-vuex

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import store from './store'
+
 import './assets/scss/main.scss'
 
+Vue.use(Vuex)
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
